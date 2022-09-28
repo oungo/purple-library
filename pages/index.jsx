cc80bea8e32fec5f6cc8a3d3970964f5eead8aa7
oungo <y2k6273@naver.com>
2022-09-28 21:51:06 +0900

    chore: eslint 설정

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+function Index() {
+  return (
+    <div className="1231312">
+      asdaslefbalwkefjawlkejfbalwkebfj awlefjbawlef
+      <div className="asdf">123awjlefhbawef awelfjawlkef</div>
+    </div>
+  );
+}
+export default Index;
