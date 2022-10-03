976156d922eb79f2944a249fdb4cf016975ccaae
oungo <y2k6273@naver.com>
2022-10-03 20:22:02 +0900

    style: add reset.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import '../styles/reset.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
