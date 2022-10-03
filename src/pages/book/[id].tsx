25d5be61c5d43f38d1ee3486efa7969ee25b6669
oungo <y2k6273@naver.com>
2022-10-03 21:32:29 +0900

    feat: 도서 상세 조회 api 호출

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,3 +1,16 @@
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
+import { IBook } from '../../types/book';
+
 export default function BookInfo() {
-  return <div>123</div>;
+  const router = useRouter();
+
+  const { data: book } = useQuery<IBook[]>(['book', router.query.id], () =>
+    axios.get(`http://localhost:8010/book/${router.query.id}`).then((res) => res.data.items)
+  );
+
+  if (!book) return null;
+
+  return <div>{book.map((info) => info.title)}</div>;
 }
