c406da8b2cfce1f22076d9bc4e76f6d816807846
oungo <y2k6273@naver.com>
2022-10-03 21:19:14 +0900

    style: 도서목록 스타일 수정

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,14 +1,31 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
+import styled from 'styled-components';
 import { useDebounce } from '../hooks/use-debounce';
 import { useKeywordStore } from '../store/useKeywordStore';
 import { IBook } from '../types/book';
+
+const BookContainer = styled.ul`
+  width: 50%;
+  margin: auto;
+  list-style: none;
+  border: 1px solid black;
+  border-top: 0;
+`;
+
+const BookItem = styled.li`
+  padding: 5px;
+  cursor: pointer;
+  :hover {
+    background-color: red;
+  }
+`;
 
 export default function BookList() {
   const keyword = useKeywordStore((state) => state.keyword);
   const newKeyword = useDebounce(keyword, 1000);
 
-  const { data } = useQuery<IBook[]>(
+  const { data: books } = useQuery<IBook[]>(
     ['books', newKeyword],
     () =>
       axios
@@ -21,11 +38,13 @@ export default function BookList() {
     }
   );
 
+  if (!books) return null;
+
   return (
-    <ul>
-      {data?.map((book) => {
-        return <li key={book.isbn}>{book.title}</li>;
+    <BookContainer>
+      {books.map((book) => {
+        return <BookItem key={book.isbn}>{book.title}</BookItem>;
       })}
-    </ul>
+    </BookContainer>
   );
 }
