f73cf13e02612307c848c396aff2edb51814e82b
oungo <y2k6273@naver.com>
2022-10-04 19:32:23 +0900

    refactor: db관련 코드 주석처리

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const db = require('./modules/index');
+// const db = require('./modules/index');
 const { getBooks, getBook } = require('./controller/book');
 
 require('dotenv').config();
@@ -12,14 +12,14 @@ app.use(cors());
 app.get('/books', getBooks);
 app.get('/book/:id', getBook);
 
-db.sequelize
-  .sync({ force: false }) // 서버 실행시마다 테이블을 재생성할건지에 대한 여부
-  .then(() => {
-    console.log('데이터베이스 연결 성공');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+// db.sequelize
+//   .sync({ force: false }) // 서버 실행시마다 테이블을 재생성할건지에 대한 여부
+//   .then(() => {
+//     console.log('데이터베이스 연결 성공');
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
 const PORT = process.env.PORT || 8010;
 app.listen(PORT, () => {
