a57204d5071e7adf1df60015d33cd9cc6206f071
oungo <y2k6273@naver.com>
2022-10-04 19:36:28 +0900

    feat: 헤더 아이콘 링크 연결

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styled from 'styled-components';
 import Nav from './Nav';
 
@@ -7,11 +8,18 @@ const Head = styled.header`
   justify-content: space-between;
   padding: 2rem 4rem;
 `;
+const Anchor = styled.a`
+  cursor: pointer;
+`;
 
 export default function Header() {
   return (
     <Head>
-      <Image src="/images/book.png" alt="아이콘" width={50} height={50} />
+      <Link href="/">
+        <Anchor>
+          <Image src="/images/book.png" alt="아이콘" width={50} height={50} />
+        </Anchor>
+      </Link>
       <Nav />
     </Head>
   );
