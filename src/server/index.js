94ba4e6baff2ebbf924759a75506a1510ccdb6f1
oungo <y2k6273@naver.com>
2022-10-10 17:27:03 +0900

    remove: 기존 db관련 소스 제거

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// const db = require('./modules/index');
 const { getBooks, getBook } = require('./controller/book');
 
 require('dotenv').config();
@@ -11,15 +10,6 @@ app.use(cors());
 
 app.get('/books', getBooks);
 app.get('/book/:id', getBook);
-
-// db.sequelize
-//   .sync({ force: false }) // 서버 실행시마다 테이블을 재생성할건지에 대한 여부
-//   .then(() => {
-//     console.log('데이터베이스 연결 성공');
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
 
 const PORT = process.env.PORT || 8010;
 app.listen(PORT, () => {
