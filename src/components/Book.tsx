86d08267434be99dd6977ff4b07d0573a737b098
oungo <y2k6273@naver.com>
2022-10-10 16:01:19 +0900

    refactor: bookInfo 컴포넌트 분리

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
 import { getBook } from '../controller/book';
-import { BookDTO, IBookResponse } from '../types/book';
+import { BookDTO, IBook, IBookResponse } from '../types/book';
 import { supabase } from '../utils/supabaseClient';
 
 const Article = styled.article`
@@ -44,46 +44,50 @@ export default function Book({ book }: IBookProps) {
 
   if (!data || error) return null;
 
-  const handleAddBook = async (bookDTO: BookDTO) => {
+  return (
+    <>
+      {data.items.map((book) => {
+        return <BookInfo key={book.isbn} book={book} />;
+      })}
+    </>
+  );
+}
+
+function BookInfo({ book }: { book: IBook }) {
+  const handleAddBook = async (book: IBook) => {
+    const { title, author, publisher, isbn } = book;
+    const bookDTO: BookDTO = {
+      title,
+      author,
+      publisher,
+      isbn,
+    };
     await supabase.from('book').insert<BookDTO>(bookDTO).throwOnError();
   };
 
   return (
-    <>
-      {data.items.map((book) => {
-        const { title, author, publisher, isbn } = book;
-        const bookDTO: BookDTO = {
-          title,
-          author,
-          publisher,
-          isbn,
-        };
-        return (
-          <Article key={book.isbn}>
-            <CoverImageSection>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src={book.image} alt="책 이미지" />
-            </CoverImageSection>
-            <InfoSection>
-              <div>
-                <Title>{book.title}</Title>
-                {book.author && <dd>작가 {book.author}</dd>}
-                {book.publisher && <dd>출판사 {book.publisher}</dd>}
-                {book.link && (
-                  <dd>
-                    <a href={book.link} target="_blank" rel="noreferrer noopener">
-                      구매 링크
-                    </a>
-                  </dd>
-                )}
-              </div>
-              <div>
-                <button onClick={() => handleAddBook(bookDTO)}>+ 사내 도서 추가</button>
-              </div>
-            </InfoSection>
-          </Article>
-        );
-      })}
-    </>
+    <Article>
+      <CoverImageSection>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={book.image} alt="책 이미지" />
+      </CoverImageSection>
+      <InfoSection>
+        <div>
+          <Title>{book.title}</Title>
+          {book.author && <dd>작가 {book.author}</dd>}
+          {book.publisher && <dd>출판사 {book.publisher}</dd>}
+          {book.link && (
+            <dd>
+              <a href={book.link} target="_blank" rel="noreferrer noopener">
+                구매 링크
+              </a>
+            </dd>
+          )}
+        </div>
+        <div>
+          <button onClick={() => handleAddBook(book)}>+ 사내 도서 추가</button>
+        </div>
+      </InfoSection>
+    </Article>
   );
 }
