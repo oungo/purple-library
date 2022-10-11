50cc13303b2f65b911880c875e71fbea7787ec69
oungo <y2k6273@naver.com>
2022-10-11 23:07:05 +0900

    feat: 네이버 도서검색 에러 컴포넌트 추가

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -8,28 +8,34 @@ const BookContainer = styled.ul`
   width: 50%;
   margin: 1rem auto 0;
   list-style: none;
-  border: 1px solid black;
   border-radius: 0.5rem;
 `;
 
 const BookItem = styled.li`
-  padding: 5px;
   cursor: pointer;
+  padding: 5px;
+  border-radius: 0.5rem;
   a {
     display: block;
   }
   :hover {
-    font-size: 1.1rem;
+    box-shadow: 0 8px 30px rgba(0, 0, 0, 0.12);
+    transition: box-shadow 0.2s;
   }
 `;
-
+const ErrorText = styled.p`
+  color: red;
+  text-align: center;
+  margin-top: 1rem;
+`;
 export default function SearchResult() {
   const keyword = useKeywordStore((state) => state.keyword);
   const newKeyword = useDebounce(keyword, 700);
 
   const { data: books, error } = useSearchResult(newKeyword);
 
-  if (!books || error) return null;
+  if (error) return <ErrorText>데이터를 조회할 수 없습니다.</ErrorText>;
+  if (!books) return null;
 
   return (
     <BookContainer>
