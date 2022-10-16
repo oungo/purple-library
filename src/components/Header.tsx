521c767bf7e5dd632e06e2c32ec7532185277239
oungo <y2k6273@naver.com>
2022-10-16 23:00:07 +0900

    style: 헤더 padding 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import styled from 'styled-components';
 import Nav from './Nav';
@@ -6,7 +5,7 @@ import Nav from './Nav';
 const Head = styled.header`
   display: flex;
   justify-content: space-between;
-  padding: 2rem 4rem;
+  padding: 1rem 4rem;
 `;
 const Anchor = styled.a`
   cursor: pointer;
@@ -17,7 +16,7 @@ export default function Header() {
     <Head>
       <Link href="/">
         <Anchor>
-          <Image src="/images/book.png" alt="아이콘" width={50} height={50} />
+          홈{/* <Image src="/images/book.png" alt="아이콘" width={50} height={50} /> */}
         </Anchor>
       </Link>
       <Nav />
