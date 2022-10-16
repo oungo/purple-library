1040ab82941ef5dc1471354efcd6b6c850c2ee5d
oungo <y2k6273@naver.com>
2022-10-16 21:25:08 +0900

    style: 도서 테이블 레이아웃 변경

--- src/components/book/Table.tsx
+++ src/components/book/Table.tsx
@@ -4,6 +4,31 @@ import { LibraryBook } from '@/types/book';
 import { useQuery } from 'react-query';
 import { PostgrestResponse } from '@supabase/postgrest-js/src/types';
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const TableWrapper = styled.div`
+  padding: 0 100px;
+`;
+const BookTable = styled.table`
+  table-layout: fixed;
+  width: 100%;
+`;
+const TableItem = styled.p`
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 5px;
+`;
+const AuthorCol = styled.col`
+  width: 15%;
+`;
+const PublisherCol = styled.col`
+  width: 15%;
+`;
+const InStockCol = styled.col`
+  width: 10%;
+`;
 
 export interface TableProps {
   books: PostgrestResponse<LibraryBook>;
@@ -21,29 +46,46 @@ export default function Table({ books }: TableProps) {
   if (!data) return null;
 
   return (
-    <table border={1}>
-      <thead>
-        <tr>
-          <th>제목</th>
-          <th>저자</th>
-          <th>출판사</th>
-          <th>상태</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.data?.map((book) => {
-          return (
-            <tr key={book.id}>
-              <td>
-                <Link href={`/book/${book.isbn}`}>{book.title}</Link>
-              </td>
-              <td>{book.author}</td>
-              <td>{book.publisher}</td>
-              <td>{book.inStock ? '보유중' : '구매 예정'}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <TableWrapper>
+      <BookTable>
+        <caption>전체 도서 목록</caption>
+        <colgroup>
+          <col></col>
+          <AuthorCol></AuthorCol>
+          <PublisherCol></PublisherCol>
+          <InStockCol></InStockCol>
+        </colgroup>
+        <thead>
+          <tr>
+            <th>제목</th>
+            <th>저자</th>
+            <th>출판사</th>
+            <th>상태</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.data?.map((book) => {
+            return (
+              <tr key={book.id}>
+                <td>
+                  <TableItem>
+                    <Link href={`/book/${book.isbn}`}>{book.title}</Link>
+                  </TableItem>
+                </td>
+                <td>
+                  <TableItem>{book.author}</TableItem>
+                </td>
+                <td>
+                  <TableItem>{book.publisher}</TableItem>
+                </td>
+                <td>
+                  <TableItem>{book.inStock ? '보유중' : '구매 예정'}</TableItem>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </BookTable>
+    </TableWrapper>
   );
 }
