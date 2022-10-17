96ddb26e73067328158899497bd00ca28cd3bc72
oungo <y2k6273@naver.com>
2022-10-17 22:47:08 +0900

    feat: 페이지네이션 이전, 다음 페이지 화살표 추가

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,17 +1,30 @@
 import { PAGE_SIZE } from '@/utils/common';
 import { useRouter } from 'next/router';
-import { MouseEvent } from 'react';
+import { MouseEvent, useMemo } from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+`;
 const PageNumbers = styled.div`
   display: flex;
-  justify-content: center;
   gap: 10px;
 `;
+const Arrow = styled.div`
+  width: 30px;
+  cursor: pointer;
+  text-align: center;
+`;
+
+const PAGE_COUNT = 10;
+
 interface PageNumberProps {
   active: boolean;
 }
-
 const PageNumber = styled.a<PageNumberProps>`
   background-color: ${(props) => (props.active ? 'var(--primary-color)' : 'white')};
   color: ${(props) => (props.active ? 'white' : 'black')};
@@ -19,23 +32,15 @@ const PageNumber = styled.a<PageNumberProps>`
   width: 30px;
   text-align: center;
   border-radius: 5px;
-  margin-top: 1rem;
 `;
 
 interface PaginationProps {
   total: number;
 }
 
-const getPageNumbers = (total: number) => {
-  const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(total / PAGE_SIZE); i++) {
-    pageNumbers.push(i);
-  }
-  return pageNumbers;
-};
-
 export default function Pagination({ total }: PaginationProps) {
   const router = useRouter();
+  const pageNumber = useMemo(() => Number(router.query.page || 1), [router.query.page]);
 
   const handleMovePage = (e: MouseEvent<HTMLDivElement>) => {
     if (e.target !== e.currentTarget) {
@@ -51,12 +56,72 @@ export default function Pagination({ total }: PaginationProps) {
   if (!total) null;
 
   return (
-    <PageNumbers onClick={handleMovePage}>
-      {getPageNumbers(total).map((page) => (
-        <PageNumber key={page} active={Number(router.query.page) === page}>
-          {page}
-        </PageNumber>
-      ))}
-    </PageNumbers>
+    <Container>
+      <PrevPageArrow pageNumber={pageNumber} />
+      <PageNumbers onClick={handleMovePage}>
+        {getPageNumbers(total)
+          .slice(getSliceStart(pageNumber), getSliceEnd(pageNumber))
+          .map((page) => (
+            <PageNumber key={page} active={pageNumber === page}>
+              {page}
+            </PageNumber>
+          ))}
+      </PageNumbers>
+      <NextPageArrow pageNumber={pageNumber} lastPage={getPageNumbers(total).length} />
+    </Container>
   );
+}
+
+function getPageNumbers(total: number) {
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(total / PAGE_SIZE); i++) {
+    pageNumbers.push(i);
+  }
+  return pageNumbers;
+}
+
+interface ArrowProps {
+  pageNumber: number;
+}
+export function PrevPageArrow({ pageNumber }: ArrowProps) {
+  const router = useRouter();
+
+  const handleMovePrev = () => {
+    if (pageNumber > 1) {
+      router.replace({
+        pathname: router.pathname,
+        query: {
+          page: pageNumber - 1,
+        },
+      });
+    }
+  };
+  return <Arrow onClick={handleMovePrev}>&lt;</Arrow>;
+}
+
+interface NextArrowProps extends ArrowProps {
+  lastPage: number;
+}
+export function NextPageArrow({ lastPage }: NextArrowProps) {
+  const router = useRouter();
+  const pageNumber = useMemo(() => Number(router.query.page), [router.query.page]);
+
+  const handleMoveNext = () => {
+    if (pageNumber < lastPage) {
+      router.replace({
+        pathname: router.pathname,
+        query: {
+          page: pageNumber + 1,
+        },
+      });
+    }
+  };
+  return <Arrow onClick={handleMoveNext}> &#62;</Arrow>;
+}
+
+function getSliceStart(pageNumber = 0) {
+  return Math.floor((pageNumber - 1) / PAGE_COUNT) * PAGE_COUNT;
+}
+function getSliceEnd(pageNumber: number) {
+  return getSliceStart(pageNumber) + PAGE_COUNT;
 }
