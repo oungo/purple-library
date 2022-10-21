2842ef8d801f7f0a49db922f7770790b0b382148
oungo <y2k6273@naver.com>
2022-10-21 22:56:56 +0900

    feat: 탭 컴포넌트 추가

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,9 +1,40 @@
+import { useRouter } from 'next/router';
+import { MouseEvent } from 'react';
+import styled from 'styled-components';
+
+const TabList = styled.ul`
+  display: flex;
+  list-style: none;
+  gap: 1rem;
+`;
+const TabItem = styled.li`
+  cursor: pointer;
+`;
+
 export default function Tabs() {
+  const router = useRouter();
+
+  const handleClickTab = (e: MouseEvent<HTMLUListElement>) => {
+    const target = e.target as HTMLUListElement;
+    if (target.tagName !== 'LI') return;
+
+    const tabValue = target.dataset.tabValue;
+
+    const query = { ...router.query, inStock: tabValue };
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const filteredQuery = Object.fromEntries(Object.entries(query).filter(([_, value]) => value));
+
+    router.replace({
+      pathname: router.pathname,
+      query: filteredQuery,
+    });
+  };
+
   return (
-    <ul>
-      <li>전체</li>
-      <li>구매 예정</li>
-      <li>보유 도서</li>
-    </ul>
+    <TabList onClick={handleClickTab}>
+      <TabItem data-tab-value={null}>전체</TabItem>
+      <TabItem data-tab-value={false}>구매 예정</TabItem>
+      <TabItem data-tab-value={true}>보유 도서</TabItem>
+    </TabList>
   );
 }
