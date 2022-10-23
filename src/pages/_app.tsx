078f2f910a5f600545253313810a950c91d2dafb
oungo <y2k6273@naver.com>
2022-10-23 23:09:48 +0900

    refactor: 레이아웃 관련 컴포넌트 폴더 이동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import '@/styles/reset.css';
 import '@/styles/global.css';
-import Layout from '@/components/Layout';
+import Layout from '@/components/layout/Layout';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient({
