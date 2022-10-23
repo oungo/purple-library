de94213aa342abaf419b1781d1be8f3175db62ec
oungo <y2k6273@naver.com>
2022-10-23 19:25:05 +0900

    refactor: 도서 테이블 body컴포넌트 분리

--- src/components/book/Table.tsx
+++ src/components/book/Table.tsx
@@ -1,12 +1,7 @@
-import * as queryKeys from '@/utils/queryKeys';
 import { definitions } from '@/types/supabase';
-import { useQuery } from 'react-query';
-import Link from 'next/link';
 import styled from 'styled-components';
-import { getBooks } from '@/utils/book/getBooks';
-import { useRouter } from 'next/router';
-import { colors } from '@/styles/color';
 import { PostgrestResponse } from '@supabase/supabase-js';
+import TableBody from './TableBody';
 
 const TableWrapper = styled.div`
   padding: 0 100px;
@@ -23,29 +18,6 @@ const Caption = styled.caption`
 const THeadTr = styled.tr`
   border-bottom: 1px solid black;
 `;
-const TBodyTr = styled.tr`
-  a {
-    display: block;
-  }
-  :hover {
-    background-color: ${colors.gray};
-  }
-`;
-const TableItem = styled.p`
-  text-align: center;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  padding: 5px;
-`;
-const Title = styled.a`
-  cursor: pointer;
-  text-align: center;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  padding: 5px;
-`;
 const AuthorCol = styled.col`
   width: 15%;
 `;
@@ -55,26 +27,12 @@ const PublisherCol = styled.col`
 const InStockCol = styled.col`
   width: 10%;
 `;
-const EditButton = styled.button`
-  cursor: pointer;
-  color: ${colors.primary};
-`;
 
 export interface TableProps {
   books: PostgrestResponse<definitions['book']>;
 }
 
 export default function Table({ books }: TableProps) {
-  const router = useRouter();
-  const query = router.query;
-
-  const { data } = useQuery([queryKeys.BOOKS, query], () => getBooks(query), {
-    initialData: books,
-    keepPreviousData: true,
-  });
-
-  if (!data) return null;
-
   return (
     <TableWrapper>
       <BookTable>
@@ -95,29 +53,7 @@ export default function Table({ books }: TableProps) {
           </THeadTr>
         </thead>
         <tbody>
-          {data.data?.map((book) => {
-            return (
-              <TBodyTr key={book.id}>
-                <td title={book.title}>
-                  <Link href={`/book/${book.isbn}`}>
-                    <Title>{book.title}</Title>
-                  </Link>
-                </td>
-                <td>
-                  <TableItem>{book.author}</TableItem>
-                </td>
-                <td title={book.publisher}>
-                  <TableItem>{book.publisher}</TableItem>
-                </td>
-                <td>
-                  <TableItem>{book.inStock ? '보유중' : '구매 예정'}</TableItem>
-                </td>
-                <td>
-                  <EditButton>수정</EditButton>
-                </td>
-              </TBodyTr>
-            );
-          })}
+          <TableBody books={books} />
         </tbody>
       </BookTable>
     </TableWrapper>
---
+++ src/components/book/TableBody.tsx
@@ -0,0 +1,81 @@
+import * as queryKeys from '@/utils/queryKeys';
+import { definitions } from '@/types/supabase';
+import { PostgrestResponse } from '@supabase/supabase-js';
+import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
+import { getBooks } from '@/utils/book/getBooks';
+import { colors } from '@/styles/color';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const TBodyTr = styled.tr`
+  a {
+    display: block;
+  }
+  :hover {
+    background-color: ${colors.gray};
+  }
+`;
+const TableItem = styled.p`
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 5px;
+`;
+const Title = styled.a`
+  cursor: pointer;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 5px;
+`;
+const EditButton = styled.button`
+  cursor: pointer;
+  color: ${colors.primary};
+`;
+
+export interface ITableBodyProps {
+  books: PostgrestResponse<definitions['book']>;
+}
+
+export default function TableBody({ books }: ITableBodyProps) {
+  const router = useRouter();
+  const query = router.query;
+
+  const { data } = useQuery([queryKeys.BOOKS, query], () => getBooks(query), {
+    initialData: books,
+    keepPreviousData: true,
+  });
+
+  if (!data) return null;
+
+  return (
+    <>
+      {data.data?.map((book) => {
+        return (
+          <TBodyTr key={book.id}>
+            <td title={book.title}>
+              <Link href={`/book/${book.isbn}`}>
+                <Title>{book.title}</Title>
+              </Link>
+            </td>
+            <td>
+              <TableItem>{book.author}</TableItem>
+            </td>
+            <td title={book.publisher}>
+              <TableItem>{book.publisher}</TableItem>
+            </td>
+            <td>
+              <TableItem>{book.inStock ? '보유중' : '구매 예정'}</TableItem>
+            </td>
+            <td>
+              <EditButton>수정</EditButton>
+            </td>
+          </TBodyTr>
+        );
+      })}
+    </>
+  );
+}
