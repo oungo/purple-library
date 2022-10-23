7e0cf1944bfc53fbc4a46c6cce23682017d99505
oungo <y2k6273@naver.com>
2022-10-23 17:23:42 +0900

    feat: 도서목록 조회시 filter 적용

--- src/utils/book/getBooks.ts
+++ src/utils/book/getBooks.ts
@@ -3,7 +3,14 @@ import { PAGE_SIZE } from '../common';
 import { ParsedUrlQuery } from 'querystring';
 
 export const getBooks = async (query: ParsedUrlQuery) => {
+  let supabaseQuery = supabase.from('book').select('*', { count: 'exact' });
+  for (const [key, value] of Object.entries(query)) {
+    if (key === 'page') continue;
+    supabaseQuery = supabaseQuery.eq(key, value);
+  }
+
   const start = (Number(query.page || 1) - 1) * PAGE_SIZE;
   const end = start + PAGE_SIZE - 1;
-  return await supabase.from('book').select('*', { count: 'exact' }).range(start, end);
+
+  return await supabaseQuery.range(start, end);
 };
