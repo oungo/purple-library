ace6c4d292e14ca9d7de209ff215c68643dd427d
oungo <y2k6273@naver.com>
2022-10-30 17:10:39 +0900

    refactor: books 페이지 제거하고 index에서 모두 보여주도록 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,38 @@
+import BookModal from '@/components/book/BookModal';
+import Pagination from '@/components/book/Pagination';
 import SearchInput from '@/components/book/SearchInput';
 import SearchResult from '@/components/book/SearchResult';
+import Table from '@/components/book/Table';
+import Tabs from '@/components/book/Tabs';
+import ModalPortal from '@/components/common/ModalPortal';
+import { DehydratedStateProps } from '@/types/common';
+import { BOOK_MODAL_ID } from '@/utils/common';
+import { getBooks } from 'api/books';
+import { GetServerSideProps } from 'next';
+import { QueryClient, dehydrate } from 'react-query';
+import * as queryKeys from '@/utils/queryKeys';
 
-function Index() {
+export default function Index() {
   return (
     <>
       <SearchInput />
       <SearchResult />
+
+      <Tabs />
+      <Table />
+      <Pagination />
+
+      <div id={BOOK_MODAL_ID} />
+      <ModalPortal id={BOOK_MODAL_ID}>
+        <BookModal />
+      </ModalPortal>
     </>
   );
 }
-export default Index;
+
+export const getServerSideProps: GetServerSideProps<DehydratedStateProps> = async (context) => {
+  const queryClient = new QueryClient();
+  await queryClient.fetchQuery([queryKeys.BOOKS, context.query], () => getBooks(context.query));
+
+  return { props: { dehydratedState: dehydrate(queryClient) } };
+};
