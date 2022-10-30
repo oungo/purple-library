5488a18053c4eb388850f8f6e1210df5eed51aa0
oungo <y2k6273@naver.com>
2022-10-30 16:08:30 +0900

    chore: next.config.js remotePatterns 추가

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'shopping-phinf.pstatic.net',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
