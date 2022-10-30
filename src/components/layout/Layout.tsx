66224af1c5ba02a134f3c01cc215709d28206b34
oungo <y2k6273@naver.com>
2022-10-30 18:06:49 +0900

    style: main 태그 margin-top 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,19 @@
+import styled from 'styled-components';
 import Header from './Header';
 
 export interface LayoutProps {
   children: React.ReactNode;
 }
 
+const Main = styled.main`
+  margin-top: 2rem;
+`;
+
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <Main>{children}</Main>
     </>
   );
 }
