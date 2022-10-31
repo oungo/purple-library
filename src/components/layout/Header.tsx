33d40c257d8a03aba70ede577c1802d54f90b71b
oungo <y2k6273@naver.com>
2022-10-31 21:03:49 +0900

    feat: 로그아웃 버튼 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,7 @@
 import { colors } from '@/styles/color';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import Search from '../book/Search';
 import Nav from './Nav';
@@ -11,8 +13,19 @@ const Head = styled.header`
   align-items: center;
   background-color: ${colors.gray};
 `;
+const LogoutButton = styled.button`
+  margin-left: 1rem;
+`;
 
 export default function Header() {
+  const supabase = useSupabaseClient();
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    router.push('/');
+  };
+
   return (
     <Head>
       <Link href="/">홈</Link>
@@ -20,6 +33,8 @@ export default function Header() {
       <Search />
 
       <Nav />
+
+      <LogoutButton onClick={handleLogout}>로그아웃</LogoutButton>
     </Head>
   );
 }
