8815c7c1e2df61c11cbd79155d4e927c4f88f6df
oungo <y2k6273@naver.com>
2022-11-01 23:26:49 +0900

    remove: TableBody 컴포넌트 불필요한 소스 제거

--- src/components/book/TableBody.tsx
+++ src/components/book/TableBody.tsx
@@ -1,29 +1,23 @@
 import * as queryKeys from '@/utils/queryKeys';
 import { useRouter } from 'next/router';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 import { colors } from '@/styles/color';
 import Link from 'next/link';
 import styled from 'styled-components';
 import { getBookStatus } from '@/utils/common';
-import { useBoundStore } from '@/store/useBoundStore';
-import { deleteBook, getBooks } from 'api/books';
+import { getBooks } from 'api/books';
 
-const TBodyTr = styled.tr`
-  a {
-    display: block;
-  }
+const Tr = styled.tr`
   :hover {
     background-color: ${colors.gray};
   }
-  td {
-    text-align: center;
-  }
 `;
-const TableItem = styled.p`
+const Td = styled.td`
+  padding: 5px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  padding: 5px;
+  text-align: center;
 `;
 const Title = styled.a`
   cursor: pointer;
@@ -37,42 +31,10 @@ const EditButton = styled.button`
   cursor: pointer;
   color: ${colors.brand};
 `;
-const DeleteButton = styled.button`
-  padding: 5px;
-  cursor: pointer;
-  color: ${colors.red};
-`;
-const EditTd = styled.td`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 5px;
-`;
 
 export default function TableBody() {
-  const queryClient = useQueryClient();
   const router = useRouter();
   const query = router.query;
-
-  const { mutate } = useMutation(deleteBook, {
-    onSuccess: () => {
-      queryClient.invalidateQueries([queryKeys.BOOKS]);
-    },
-  });
-
-  const open = useBoundStore((state) => state.open);
-  const setId = useBoundStore((state) => state.setId);
-
-  const handleOpen = (id: number) => {
-    setId(id);
-    open();
-  };
-
-  const handleDelete = (id: number) => {
-    if (confirm('도서를 삭제하시겠습니까?')) {
-      mutate(id);
-    }
-  };
 
   const { data } = useQuery([queryKeys.BOOKS, query], () => getBooks(query), {
     keepPreviousData: true,
@@ -84,32 +46,21 @@ export default function TableBody() {
     <>
       {data.data?.map((book) => {
         return (
-          <TBodyTr key={book.id}>
-            <td title={book.title}>
+          <Tr key={book.id}>
+            <Td title={book.title}>
               <Link href={`/book/${book.isbn}`}>
                 <Title>{book.title}</Title>
               </Link>
-            </td>
-            <td>
-              <TableItem>{book.author}</TableItem>
-            </td>
-            <td title={book.publisher || ''}>
-              <TableItem>{book.publisher}</TableItem>
-            </td>
-            <td>
-              <TableItem>{getBookStatus(book.inStock)}</TableItem>
-            </td>
-            <td>
-              <TableItem>{book.discount}</TableItem>
-            </td>
-            <td>
-              <TableItem>{book.buyer}</TableItem>
-            </td>
-            <EditTd>
-              <EditButton onClick={() => handleOpen(book.id)}>수정</EditButton>
-              <DeleteButton onClick={() => handleDelete(book.id)}>삭제</DeleteButton>
-            </EditTd>
-          </TBodyTr>
+            </Td>
+            <Td>{book.author}</Td>
+            <Td title={book.publisher || ''}>{book.publisher}</Td>
+            <Td>{getBookStatus(book.inStock)}</Td>
+            <Td>{book.discount}</Td>
+            <Td>{book.buyer}</Td>
+            <Td>
+              <EditButton>수정</EditButton>
+            </Td>
+          </Tr>
         );
       })}
     </>
