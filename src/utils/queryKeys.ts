f893caf96e164a9c6dec17893be389617ba918e9
oungo <y2k6273@naver.com>
2022-11-07 09:18:03 +0900

    feat: user 호출 함수 추가

--- src/utils/queryKeys.ts
+++ src/utils/queryKeys.ts
@@ -4,3 +4,5 @@ export const BOOKS = 'books';
 export const BOOK = 'book';
 export const STOCK_BOOK_COUNT = 'stockBookCount';
 export const TO_PURCHASE_BOOK_COUNT = 'toPurchaseBookCount';
+
+export const USER = 'user';
