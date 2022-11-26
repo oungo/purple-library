c919b66bea7cea44a6b4f5dfbeb06c5f73431c4e
oungo <y2k6273@naver.com>
2022-11-26 21:55:03 +0900

    feat: 회원가입시인증메일 발송 안내

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -1,5 +1,5 @@
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
-import { MouseEvent } from 'react';
+import { MouseEvent, useState } from 'react';
 import Button from '../common/Button';
 import Input from '../common/Input';
 import Label from '../common/Label';
@@ -11,6 +11,8 @@ interface SignUpFormProps {
 }
 
 export default function SignUpForm({ onError }: SignUpFormProps) {
+  const [successSendAuthMail, setSuccessSendAuthMail] = useState(false);
+
   const supabase = useSupabaseClient();
 
   const handleSubmit = async (e: MouseEvent<HTMLFormElement>) => {
@@ -18,7 +20,10 @@ export default function SignUpForm({ onError }: SignUpFormProps) {
     const formData = new FormData(e.target as HTMLFormElement);
     const formValue = Object.fromEntries(formData) as AuthFormValue;
 
-    const { error } = await supabase.auth.signUp(formValue);
+    const { data, error } = await supabase.auth.signUp(formValue);
+    if (data) {
+      setSuccessSendAuthMail(true);
+    }
     if (error) {
       onError(error.message);
     }
@@ -30,15 +35,15 @@ export default function SignUpForm({ onError }: SignUpFormProps) {
         <Label htmlFor="email">Email</Label>
         <Input type="text" name="email" id="email" fullWidth />
       </FormItem>
-
       <FormItem>
         <Label htmlFor="password">Password</Label>
         <Input type="password" name="password" fullWidth />
       </FormItem>
-
       <Button fullWidth buttonType="primary" type="submit">
         회원 가입
       </Button>
+
+      {successSendAuthMail && <p>인증 메일을 발송했습니다. 메일을 확인해주세요.</p>}
     </form>
   );
 }
