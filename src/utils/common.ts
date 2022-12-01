e35f0814aa5e4d262687b63ae3811c6e8a82a820
oungo <y2k6273@naver.com>
2022-12-01 23:29:15 +0900

    feat: 유저 모달id 추가

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -6,6 +6,7 @@ export const axiosInstance = axios.create({
 
 export const PAGE_SIZE = 15;
 export const BOOK_MODAL_ID = 'bookModal';
+export const USER_MODAL_ID = 'userModal';
 
 export const getBookStatus = (inStock?: boolean) => {
   return inStock ? '보유중' : '구매 예정';
