2576d4c857face9e4b41a5e52e389e580cfbe1d1
oungo <y2k6273@naver.com>
2022-12-06 23:12:42 +0900

    feat: 사용자 목록 조회 api count, order, range 추가

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,12 +1,28 @@
 import { PartialUser } from '@/types/user';
+import { PAGE_SIZE } from '@/utils/common';
 import { supabase } from '@/utils/supabaseClient';
+import { ParsedUrlQuery } from 'querystring';
 
 export const getUser = async (uid?: string) => {
   return supabase.from('user').select('*').eq('uid', uid).throwOnError().single();
 };
 
-export const getUsers = async () => {
-  return supabase.from('user').select('*').order('id', { ascending: false }).throwOnError();
+export const getUsers = async (query: ParsedUrlQuery = {}) => {
+  let supabaseQuery = supabase
+    .from('user')
+    .select('*', { count: 'exact' })
+    .order('id', { ascending: false })
+    .throwOnError();
+
+  for (const [key, value] of Object.entries(query)) {
+    if (key === 'page') continue;
+    supabaseQuery = supabaseQuery.eq(key, value as string);
+  }
+
+  const start = (Number(query.page || 1) - 1) * PAGE_SIZE;
+  const end = start + PAGE_SIZE - 1;
+
+  return await supabaseQuery.range(start, end);
 };
 
 export const updateUser = async (values: PartialUser) => {
