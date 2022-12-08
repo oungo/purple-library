75a6e43cb8170f13333984da0b19981985b965e4
oungo <y2k6273@naver.com>
2022-12-09 00:54:58 +0900

    feat: 이메일이나 이름같을경우 반납 버튼 노출

--- src/components/book/BookTable.tsx
+++ src/components/book/BookTable.tsx
@@ -100,7 +100,7 @@ export default function BookTable() {
       width: '10%',
       render: (_, { id, inStock, lender }) => {
         if (inStock) {
-          return user?.data?.email === lender ? (
+          return user?.data?.email === lender || user?.data?.name === lender ? (
             <Button size="small" onClick={() => handleChangeBookStatus({ id, lender: '' })}>
               반납
             </Button>
